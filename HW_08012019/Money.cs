ef4b8ce9f666fdb4d86cb30811747c448f94063f
Омирзаков Маргулан Толеубекулы <@corp.itstep>
2019-03-27 09:47:45 +0600

    Signed-off-by: Омирзаков Маргулан Толеубекулы <ОмирзаковМ@corp.itstep>

---
+++ HW_08012019/Money.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_08012019
+{
+    class Money
+    {
+        private int nominal; // Номинал купюры
+        private int count; // Колличество купюр
+
+        public Money(int banknotes, int number)
+        {
+            nominal = banknotes;
+            count = number;
+        }
+        public void ShowInfoMoney(Money[] purse)
+        {
+            // Цикл выводящий номинал и колличество купюр
+            foreach (Money i in purse)
+            {
+                Console.WriteLine("Купюра номиналом: {0} T. - {1} шт.",
+                i.nominal, i.count);
+            }
+        }
+        public bool BuyOne(Money[] purse, int price) //Хватает ли денежных средств на покупку
+        {
+            return (ShowAllMoney(purse) >= price);
+        }
+        public int BuyAll(Money[] purse, int price) //Сколько шт. можно купить на деньги
+        {
+            return (ShowAllMoney(purse) / price);
+        }
+        public int ShowAllMoney(Money[] purse)
+        {
+            // Переменная для хранения суммы банкнот
+            int sum = 0;
+
+            // Цикл считающий сумму всех банкнот
+            foreach (Money i in purse)
+            {
+                // Сумма увеличивается на произведение номинала на колличество
+                sum += i.nominal * i.count;
+            }
+
+            //Вернуть сумму
+            return sum;
+        }
+        public int Banknote         //Свойства: получает значение полей
+        {
+            get
+            {
+                // Вернуть номинал банкноты
+                return nominal;
+            }
+
+            set
+            {
+                // Установить номинал банкноты
+                nominal = value;
+            }
+        }
+        public int Number             // Свойства: чтение номинала банкнот и колличества
+        {
+            get
+            {
+                // Вернуть номинал банкноты
+                return count;
+            }
+
+
+        }
+        public int AllMoneyBanknote                         //Свойство расчитать сумму денег
+        {
+            get
+            {
+                //Вернуть сумму банкнот данного номинала
+                return nominal * count;
+            }
+        }
+        public static Money operator ++(Money t)
+        {//перегрузка  ++
+            t.nominal = t.nominal + 1;
+            t.count = t.count + 1;
+            return t;
+        }
+        public static Money operator --(Money t)
+        {//перегрузка --
+            t.nominal = t.nominal - 1;
+            t.count = t.count - 1;
+            return t;
+        }
+        public static bool operator !(Money t)
+        {//перегрузка !
+            if (t.count != 0) return true;
+            else return false;
+        }
+        public static Money operator +(Money t, int skalyar)
+        {//перегрузка +
+            t.count += skalyar;
+            return t;
+        }
+        public static explicit operator string(Money t)
+        {
+            return t.nominal + " " + t.nominal;
+        }
+        public static explicit operator Money(string t)
+        { //перегрузка- money в string
+            string[] m = t.Split(' ');
+            Money M = new Money(int.Parse(m[0]), int.Parse(m[1]));
+            return M;
+        }
+    }
+}
---
+++ HW_08012019/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_08012019
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Создание массива кошелька и наполнения его банкнотами
+            Money[] dengi = new Money[4];
+            dengi[0] = new Money(50, 4);
+            dengi[1] = new Money(100, 8);
+            dengi[2] = new Money(500, 2);
+            dengi[3] = new Money(1000, 3);
+
+            // Вывести информацию о всех банкнотах
+            dengi[0].ShowInfoMoney(dengi);
+
+            // Вывести сумму всех банкнот
+            Console.WriteLine("В вашем кошельке: {0} тг.", dengi[0].ShowAllMoney(dengi));
+
+            // Хватит ли денег на покупку
+            if (dengi[0].BuyOne(dengi, 3500))
+                Console.WriteLine("На оплату товара, денежных стредств хватает");
+            else
+                Console.WriteLine("На оплату товара, денег НЕ хватает");
+
+            // Купить на все деньги
+            Console.WriteLine("На {0} можно купть {1} ед. товара по 330",
+            dengi[0].ShowAllMoney(dengi), dengi[0].BuyAll(dengi, 330));
+
+            // Свойства: чтение-запись номинала банкнот и колличества,
+            // вывод суммы всех банкнот данного номинала
+            Console.WriteLine("\nНоминал банкнот: {0}\n" +
+            "Колличество банкнот: {1}\n" +
+            "Сумма всех банкнот данного номинала: {2}",
+           dengi[3].Banknote, dengi[3].Number, dengi[3].AllMoneyBanknote);
+            dengi[3].Banknote = 5000;
+
+
+            Console.WriteLine("\nНоминал банкнот: {0}\n" +
+            "Колличество банкнот: {1}\n" +
+            "Сумма всех банкнот данного номинала: {2}",
+           dengi[3].Banknote, dengi[3].Number, dengi[3].AllMoneyBanknote);
+
+            dengi[0]++;
+
+            //вывод
+            Money M = new Money(1, 1);
+            string s = (string)M;
+            Console.WriteLine(s);
+            string s1 = "2 2";
+            Money M1 = (Money)s1;
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
